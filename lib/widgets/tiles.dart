ae9a40e0c2ab76baf716920111d9dea5bedc5c63
Darlington Hapanamambo <108944518+darlingtonhp@users.noreply.github.com>
2024-11-13 12:48:14 +0200

    Update tiles.dart

--- lib/widgets/tiles.dart
+++ lib/widgets/tiles.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class ScanResultTile extends StatelessWidget {
+class ScanResultCard extends StatelessWidget {
   final String description;
   final IconData icon;
   final String scanResult;
 
-  const ScanResultTile({
+  const ScanResultCard({
     Key? key,
     required this.description,
     required this.icon,
@@ -14,17 +14,33 @@ class ScanResultTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      tileColor: Colors.blue,
-      leading: Icon(
-        icon,
-        color: Colors.white,
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
       ),
-      title: Text(
-        description,
-        style: const TextStyle(color: Colors.white),
+      child: ListTile(
+        tileColor: Colors.blue[50],
+        leading: Icon(
+          icon,
+          color: Colors.blueAccent,
+        ),
+        title: Text(
+          description,
+          style: const TextStyle(
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          scanResult,
+          style: TextStyle(
+            color: Colors.blue[700],
+            fontSize: 16,
+          ),
+        ),
       ),
-      subtitle: Text(scanResult),
     );
   }
 }
